a69a54ec440bd2a36fb9bbd4d5bed055e69f18b7
Max Rydahl Andersen <manderse@redhat.com>
2015-12-01 11:23:57 +0100

    Issue #29 Add red hat internal docker-registry
    * Needed to pull and push from internal repo to share
      and get access to shared docker images using rhel & eap

--- cdk-v2/scripts/configure_docker.sh
+++ cdk-v2/scripts/configure_docker.sh
@@ -10,12 +10,6 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
-# The Docker registry from where we pull the OpenShift Enterprise Docker image
-REDHAT_DOCKER_REGISTRY="registry.access.redhat.com"
-
-# TODO - Deployer and router come from this repo. For now it is needed.
-REDHAT_INTERNAL_DOCKER_REGISTRY="rcm-img-docker01.build.eng.bos.redhat.com:5001"
-
 if [ -f /opt/docker_selinux ]; then
 	echo "[INFO] Skipping Docker configuration. Already done."
 	exit 0;
@@ -27,13 +21,20 @@ sudo setenforce 0
 sudo touch /opt/docker_selinux
 
 # Confiugure the Docker registry to get access to the needed repositories
-echo "[INFO] Enabling Docker registries ${REDHAT_DOCKER_REGISTRY} and ${REDHAT_INTERNAL_DOCKER_REGISTRY}"
+echo "[INFO] Enabling Docker registries"
 cat << EOF > /etc/sysconfig/docker
 # Configured by Vagrant
 DOCKER_CERT_PATH=/etc/docker
-INSECURE_REGISTRY='--insecure-registry ${REDHAT_INTERNAL_DOCKER_REGISTRY} --insecure-registry 172.30.0.0/16'
 OPTIONS="--selinux-enabled -H tcp://0.0.0.0:2376 -H unix:///var/run/docker.sock --tlscacert=/etc/docker/ca.pem --tlscert=/etc/docker/server-cert.pem --tlskey=/etc/docker/server-key.pem --tlsverify"
-ADD_REGISTRY='--add-registry ${REDHAT_DOCKER_REGISTRY} --add-registry ${REDHAT_INTERNAL_DOCKER_REGISTRY}'
+
+# INSECURE_REGISTRY and ADD_REGISTRY are used by systemcdl scripts. They are a
+# RHEL feature. See http://rhelblog.redhat.com/2015/04/15/understanding-the-changes-to-docker-search-and-docker-pull-in-red-hat-enterprise-linux-7-1/
+#
+# registry.access.redhat.com - registry from where we pull the OpenShift Enterprise Docker image
+# rcm-img-docker01.build.eng.bos.redhat.com:5001 - Deployer and router come from this repo. For now it is needed.
+# docker-registry.usersys.redhat.com - allow push/pull from docker-registry.usersys.redhat.com
+ADD_REGISTRY='--add-registry registry.access.redhat.com --add-registry docker-registry.usersys.redhat.com --add-registry rcm-img-docker01.build.eng.bos.redhat.com:5001'
+INSECURE_REGISTRY='--insecure-registry rcm-img-docker01.build.eng.bos.redhat.com:5001 --insecure-registry 172.30.0.0/16 --insecure-registry docker-registry.usersys.redhat.com'
 EOF
 
 systemctl restart docker
